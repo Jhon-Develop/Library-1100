427d20b19aec5600aa6481c0a144a68b16daecd4
Jhon-Develop <asprillajhon73@gmail.com>
2024-08-22 06:44:07 -0500

    add property in user

--- Models/User.cs
+++ Models/User.cs
@@ -9,6 +9,7 @@ namespace library_1100.Models
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+        public string? Password { get; set; }
         public string? Address { get; set; }
         public string? DocumentTypeId { get; set; }
         public string? DocumentNumber { get; set; }
